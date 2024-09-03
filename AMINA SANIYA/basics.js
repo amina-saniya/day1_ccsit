9a516e0a969e09b2aff894fcd6f570162a746c31
amina-saniya <aminasaniya53@gmail.com>
2024-09-03 10:46:52 +0530

    Add files via upload

---
+++ AMINA SANIYA/basics.js
@@ -0,0 +1,11 @@
+console.log("Temperature")
+temperature = 45
+if (temperature < 20) {
+    console.log("cold")
+}
+else if (temperature <= 20 && temperature >= 30) {
+    console.log("normal")
+}
+else {
+    console.log("hot")
+}
